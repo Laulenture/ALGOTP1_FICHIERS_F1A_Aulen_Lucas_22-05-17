5fbd3b877d84abfd18a8a2df06514cc754bcbfbe
LAULENOS-PORTAB\Hikaru <l.aulen@ludus-academie.com>
2017-05-22 18:02:05 +0200

    no message

---
+++ ALGOTP1_FICHIERS_F1A_AULEN_LUCAS.pas
@@ -0,0 +1,36 @@
+﻿program traitement_fichier_algo;
+
+uses crt;
+
+VAR
+	
+PROCEDURE aff_fichier () //Cette procedure affiche les contenu des fichiers
+
+	VAR
+		
+
+	BEGIN
+		
+	END;
+
+PROCEDURE groupe_verbe () //cette procedure permet de reconnaître le groupe de chaque verbe
+	
+	VAR
+		
+
+	BEGIN
+		
+	END;
+	
+PROCEDURE conjug_verbe () //cette procedure conjugue le verbe
+
+	VAR
+		
+
+	BEGIN
+		
+	END;
+	
+BEGIN
+
+END.
